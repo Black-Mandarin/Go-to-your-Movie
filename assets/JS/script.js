e519fc4e09732c9fe91d3ab5c20966b1dd8807bf
Sanjeev Kumar <sanjeevkumar@me.com>
2022-01-11 16:07:38 +1100

    Putting about us content using jquery javascript.

---
+++ assets/JS/script.js
@@ -0,0 +1,57 @@
+// About Us
+
+$(document).on('click', '#aboutUs', function (event) {
+    event.preventDefault();
+    var footer = $('footer')
+
+    // var wind = ;
+    var aboutUsContentParent = $('<div>');
+    aboutUsContentParent.attr('class', 'ui segment');
+    aboutUsContentParent.attr('id', 'aboutUsContentParent');
+    // aboutUsContent.text("Hello we are here.");
+
+    // Adding Header for About US
+    var aboutUsHeader = $('h1');
+    aboutUsHeader.attr('class', 'aboutUsContentChild');
+    aboutUsHeader.text('Project : Go to your Movie');
+    aboutUsContentParent.append(aboutUsHeader);
+
+    // Adding project member in apphabetical order
+    var aboutUsGroup = $('<h3>');
+    aboutUsGroup.attr('class', 'aboutUsContentChild');
+    aboutUsGroup.text('Alan Cherian : Developer');
+    aboutUsContentParent.append(aboutUsGroup);
+
+    // Adding project member in apphabetical order
+    var aboutUsGroup = $('<h3>');
+    aboutUsGroup.attr('class', 'aboutUsContentChild');
+    aboutUsGroup.text('Erin Hatherell : Developer');
+    aboutUsContentParent.append(aboutUsGroup);
+
+    // Adding project member in apphabetical order
+    var aboutUsGroup = $('<h3>');
+    aboutUsGroup.attr('class', 'aboutUsContentChild');
+    aboutUsGroup.text('Mohammed Mazahim : Developer');
+    aboutUsContentParent.append(aboutUsGroup);
+
+    // Adding project member in apphabetical order
+    var aboutUsGroup = $('<h3>');
+    aboutUsGroup.attr('class', 'aboutUsContentChild');
+    aboutUsGroup.text('Sanjeev Kumar : Developer');
+    aboutUsContentParent.append(aboutUsGroup);
+
+    // Adding project project description
+    var aboutUsProjectDesc = $('<p>');
+    aboutUsProjectDesc.attr('class', 'aboutUsContentChild');
+    aboutUsProjectDesc.text('We are group of four developers collaborating on project "Go to your Movies". We intend to make end user life simple by providing a web application which takes minimum input and help him/her go its favourite movie to its nearest choice of theatre.');
+    aboutUsContentParent.append(aboutUsProjectDesc);
+
+    // Adding project project appliaction URL and Repository URL 
+    var aboutUsProjectRepositoryURL = $('<a>');
+    aboutUsProjectRepositoryURL.attr('class', 'aboutUsContentChild');
+    aboutUsProjectRepositoryURL.attr('href', 'https://github.com/Black-Mandarin/Go-to-your-Movie');
+    aboutUsProjectRepositoryURL.text('Repository URL');
+    aboutUsContentParent.append(aboutUsProjectRepositoryURL);
+
+    footer.before(aboutUsContentParent);
+});
